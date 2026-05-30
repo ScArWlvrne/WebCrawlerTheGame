097d27f1976cbc118c647c2eb14cbe5b0bf54f79
Arnav De <arnavde@wless-10-108-239-128.wless.uoregon.edu>
2026-05-30 14:52:07 -0700

    increased test harness readability

--- Assets/Scripts/GameStateManager/FlagTester.cs
+++ Assets/Scripts/GameStateManager/FlagTester.cs
@@ -7,16 +7,17 @@ public class FlagTester : MonoBehaviour
         GameStateManager.Instance.SetFlag("joe_facebook_viewed", true);
 
         GameStateManager.Instance.SetTrust("lily", 50);
+
         GameStateManager.Instance.AddTrust("lily", 10);
 
         GameStateManager.Instance.SaveGame();
 
         GameStateManager.Instance.State = new GameState();
 
-        Debug.Log(GameStateManager.Instance.GetTrust("lily"));
+        Debug.Log("Trust before load: " + GameStateManager.Instance.GetTrust("lily"));
 
         GameStateManager.Instance.LoadGame();
 
-        Debug.Log(GameStateManager.Instance.GetTrust("lily"));
+        Debug.Log("Trust after load: " + GameStateManager.Instance.GetTrust("lily"));
     }
 }

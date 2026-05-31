5a2f92a1f521b7657b7d26787064c0cc7c2836e9
ScArWlvrne <kmccown@scarwlvrne.com>
2026-05-30 20:51:19 -0700

    Dear lord please let this work

---
+++ Assets/Scripts/GameStateManager/GameCharacters.cs
@@ -0,0 +1,7 @@
+public static class GameCharacters
+{
+    public const string Lily = "lily";
+    public const string Joe = "joe";
+    public const string CEOAssistant = "ceo_assistant";
+    public const string WebInspector = "web_inspector";
+}
